d39ca9f302639f0156be22808bedd9a88348f998
longlin10086 <gnulonglin@gmail.com>
2024-03-05 23:16:44 +0800

    fix: 修复一些部署相关bug

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,5 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . /usr/src/speak-insincerely/
 
-ARG USER_API_KEY=default
-ENV OPENAI_API_KEY=${USER_API_KEY}
 
 CMD [ "python", "app.py" ]
--- script/startup.sh
+++ script/startup.sh
@@ -5,6 +5,6 @@ read -r API_KEY
 
 
 echo "---------------------start to build-----------------------------"
-docker build --tag speak-insincerely:v1 --build-arg USER_API_KEY="${API_KEY}" .
+docker build --tag speak-insincerely:v1 .
 echo "---------------------building finished-----------------------------"
-docker run -d -p 7860:7860 speak-insincerely:v1
+docker run -d -p 7860:7860 -e OPENAI_API_KEY="${API_KEY}" speak-insincerely:v1
