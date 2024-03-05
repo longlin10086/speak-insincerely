01650b6657573f4e258a6b461367941353da6413
longlin10086 <gnulonglin@gmail.com>
2024-03-05 22:49:39 +0800

    fix: 修复一些部署相关bug

--- script/startup.sh
+++ script/startup.sh
@@ -7,4 +7,4 @@ read -r API_KEY
 echo "---------------------start to build-----------------------------"
 docker build --tag speak-insincerely:v1 --build-arg USER_API_KEY="${API_KEY}" .
 echo "---------------------building finished-----------------------------"
-docker run -d -p 7860:7860 -v /usr/src/speak-insincerely:/usr/src/speak-insincerely speak-insincerely:v1
+docker run -d -p 7860:7860 speak-insincerely:v1
